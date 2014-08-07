5d01c75b9421ad687a80446dec7913bd96cb78a1
Kevin Zhang <zhangyongjiang@yahoo.com>
2014-08-06 20:21:41 -0700

    import

---
+++ src/main/java/com/test/domain/UserManagerImpl.java
@@ -0,0 +1,39 @@
+package com.test.domain;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.test.domain.interfaces.UserManager;
+import com.test.storage.interfaces.UserDao;
+
+@Service
+@Transactional
+public class UserManagerImpl implements UserManager {
+
+	@Autowired
+	private UserDao userDAO;
+
+	@Override
+	public void insertUser(User user) {
+		this.userDAO.insertUser(user);
+	}
+
+	@Override
+	public void insertUserAndFailTransation(User user) {
+		this.userDAO.insertUser(user);
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public User getUser(String username) {
+		return this.userDAO.getUser(username);
+	}
+
+	@Override
+	public List<User> getUsers() {
+		return this.userDAO.getUsers();
+	}
+}
